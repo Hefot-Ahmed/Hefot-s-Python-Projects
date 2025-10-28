41a7a2b2356ad1d73e2dc197d81aa3df5d34f041
Hefot Ahmed <hefot6@gmail.com>
2025-10-27 21:57:00 -0400

    Add Pythagorean theorem calculation script
    This program calculates the length of the hypotenuse of a right triangle based on user input for the two short sides.

---
+++ variables/Pythagorean_Theroem.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the length of the short side "))
+b = int(input("Enter the length of another short side "))
+c = (a ** 2 + b ** 2) ** 0.5
+print(c)
+print("The length of the hypotenuse is", c)
+# This program calculates the length of the hypotenuse of a right triangle

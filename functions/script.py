88684e2bdebe8635bc0a38ac3233dc36252c417f
Hefot Ahmed <hefot6@gmail.com>
2025-11-01 13:16:55 -0400

    Add initial script with basic functions

---
+++ functions/script.py
@@ -0,0 +1,11 @@
+# This was the first function introduced in the course
+print('Hello World!')
+
+# This function calculates the maximum of two numbers
+print(max(3, 5))
+
+# Convert an integer number to a binary string 
+print(bin(3))
+
+# Return the absolute value of a number
+print(abs(5))

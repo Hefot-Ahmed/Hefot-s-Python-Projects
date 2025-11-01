2cc6e853d3e30c751862e0a548d9b2591b15b3b7
Hefot Ahmed <hefot6@gmail.com>
2025-10-31 20:04:55 -0400

    Add initial To-Do list implementation

---
+++ lists/To_Do_List.py
@@ -0,0 +1,12 @@
+To_Do = ['Get quarters.', 
+        'Do laundry.',
+        'Take a walk.',
+        'Get a haircut.',
+        'Make some tea.',
+        'Complete Lists chapter.',
+        'Call mom.',
+        'Watch My Hero Academia.']
+
+print(To_Do[0])
+print(To_Do[1])
+print(To_Do[2:5])

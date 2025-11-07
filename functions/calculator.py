33284efabbc3b0106de3a87c7b763ca33f3c1e91
Hefot Ahmed <hefot6@gmail.com>
2025-11-07 13:16:43 -0500

    Update calculator.py

--- functions/calculator.py
+++ functions/calculator.py
@@ -17,3 +17,33 @@ def divide (a, b):
 def exp(a, b):
   answer = a ** b
   return answer
+
+def add(a, b):
+    answer = a + b
+    return answer
+
+
+def subtract(a, b):
+    answer = a - b
+    return answer
+
+
+def multiply(a, b):
+    answer = a * b
+    return answer
+
+
+def divide(a, b):
+    answer = a / b
+    return answer
+
+
+def exp(a, b):
+    answer = a ** b
+    return answer
+
+print(add(2, 3))
+print(subtract(5, 2))
+print(multiply(4, 3))
+print(divide(10, 2))
+print(exp(2, 3))

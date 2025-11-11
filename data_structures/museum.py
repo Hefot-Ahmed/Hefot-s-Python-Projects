5cb9a0171cf2e4361edb05b32c1e02a3a8fddd80
Hefot Ahmed <hefot6@gmail.com>
2025-11-11 15:38:36 -0500

    Add museum artifact catalog with print statements

---
+++ data_structures/museum.py
@@ -0,0 +1,11 @@
+artifact_catalog = {'Artist': 'Govardhan', 
+'Period': '17th century', 'Date': '1630'}
+
+print("Artist:", artifact_catalog['Artist'])
+print("Period}:", artifact_catalog['Period'])
+print("Date:", artifact_catalog['Date'])
+
+print(artifact_catalog)
+print('Keys:', artifact_catalog.keys())
+print('Values:', artifact_catalog.values())
+print('Items:', artifact_catalog.items())

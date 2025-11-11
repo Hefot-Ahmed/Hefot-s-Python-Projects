2484f1d009d0f3220b97cf47892f95d8c3087222
Hefot Ahmed <hefot6@gmail.com>
2025-11-11 15:06:42 -0500

    Add friend locations and print combined coordinates

---
+++ data_structures/find_my_friends.py
@@ -0,0 +1,6 @@
+friend1_location = (40.7128, 74.0060) #New York City
+friend2_location = (36.1716, 115.1391) #Las Vegas
+friend3_location = (34.0549, 118.2426) #Los Angeles
+
+all_friends_locations = friend1_location + friend2_location + friend3_location
+print("All friends locations combined:", all_friends_locations)

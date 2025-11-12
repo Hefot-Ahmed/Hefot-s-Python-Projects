f53a514f0852e81fbef07e285e688892016b1f2f
Hefot Ahmed <hefot6@gmail.com>
2025-11-12 12:32:01 -0500

    Add fruit set operations for union, intersection, difference

---
+++ data_structures/fruit.py
@@ -0,0 +1,10 @@
+list1 = {'apple', 'banana', 'grapes'}
+list2 = {'oranges', 'pears', 'pineapple'}
+
+union_result = list1.union(list2)
+intersection_result = list1.intersection(list2)
+difference_result = list1.difference(list2)
+
+print(union_result)
+print(intersection_result)
+print(difference_result)

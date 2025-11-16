ffb72902a2cec8a6b461864caaeaa9e4e19ac7d8
Hefot Ahmed <hefot6@gmail.com>
2025-11-15 20:04:31 -0500

    Fix typo and update diary entry text

--- File-and-error-handling/dear_diary.py
+++ File-and-error-handling/dear_diary.py
@@ -1,4 +1,3 @@
 file = open('diaries.txt', 'w')
-file.write("Date: November 12, 2025 \n") 
-file.write("Dear Diary, today was a pretty regular day. I woke up, got ready, and went to school. The walk from the subway station wasn't too bad, but it was a bit cold this morning.
-Once I got to campus, I went to my classes, which were pretty good. After my last class finsihed, I packed up my backpack and started heading home. I'm now just relaxing and unwinding before tackling my homework.
+file.write("Date: November 12, 2025 \n")
+file.write("Dear Diary, today was a pretty regular day. I woke up, got ready, and went to school. The walk from the subway station wasn't too bad, but it was a bit cold this morning. Once I got to campus, I went to my classes, which were pretty good. After my last class finished, I packed up my backpack and started heading home. I'm now just relaxing and unwinding before tackling my homework. Overall, it was a decent day.")

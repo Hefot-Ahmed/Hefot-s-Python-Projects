afaa120810e9295659a4c0e2ec4d6cf3487c4366
Hefot Ahmed <hefot6@gmail.com>
2025-11-15 20:02:49 -0500

    Update dear_diary.py

--- File-and-error-handling/dear_diary.py
+++ File-and-error-handling/dear_diary.py
@@ -1,3 +1,4 @@
 file = open('diaries.txt', 'w')
 file.write("Date: November 12, 2025 \n") 
-file.write("Dear Diary, I went to school, ate some food on campus, and then took the bus home") 
+file.write("Dear Diary, today was a pretty regular day. I woke up, got ready, and went to school. The walk from the subway station wasn't too bad, but it was a bit cold this morning.
+Once I got to campus, I went to my classes, which were pretty good. After my last class finsihed, I packed up my backpack and started heading home. I'm now just relaxing and unwinding before tackling my homework.
